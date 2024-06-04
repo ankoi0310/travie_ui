9b4312a8c5b8a11f4e876fc27665f5fc028fb75b
koi <huynhvahuuan3620@gmail.com>
2024-06-05 02:26:54 +0700

    fix: change to full screen with margin app bar

--- app/src/main/java/vn/edu/hcmuaf/fit/travie/core/common/view/BaseActivity.java
+++ app/src/main/java/vn/edu/hcmuaf/fit/travie/core/common/view/BaseActivity.java
@@ -2,7 +2,6 @@ package vn.edu.hcmuaf.fit.travie.core.common.view;
 
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
@@ -15,8 +14,16 @@ public class BaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
 
-        Window window = getWindow();
-        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
     }
 }
--- app/src/main/java/vn/edu/hcmuaf/fit/travie/hotel/ui/view/HotelDetailActivity.java
+++ app/src/main/java/vn/edu/hcmuaf/fit/travie/hotel/ui/view/HotelDetailActivity.java
@@ -1,26 +1,17 @@
 package vn.edu.hcmuaf.fit.travie.hotel.ui.view;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.view.WindowInsets;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.widget.Toolbar;
-
-import com.google.android.material.appbar.AppBarLayout;
-
-import io.noties.markwon.Markwon;
 import vn.edu.hcmuaf.fit.travie.R;
 import vn.edu.hcmuaf.fit.travie.core.common.view.BaseActivity;
 import vn.edu.hcmuaf.fit.travie.core.shared.utils.AppUtil;
 import vn.edu.hcmuaf.fit.travie.databinding.ActivityHotelDetailBinding;
+import vn.edu.hcmuaf.fit.travie.hotel.ui.view.components.HotelDetailInfo;
 
 public class HotelDetailActivity extends BaseActivity {
     ActivityHotelDetailBinding binding;
-
-    private AppBarLayout appBarLayout;
-    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,38 +19,26 @@ public class HotelDetailActivity extends BaseActivity {
         binding = ActivityHotelDetailBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.viewMap.viewTxt.setText(R.string.view_map);
+        // add fragment hotel detail info
+        HotelDetailInfo hotelDetailInfo = HotelDetailInfo.newInstance();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.hotelDetailInfo, hotelDetailInfo)
+                .commit();
 
-        final Markwon markwon = Markwon.create(this);
-        String introductionText = "Emphasis, aka italics, with *asterisks* or _underscores_.\n\n" +
-                "Strong emphasis, aka bold, with **asterisks** or __underscores__.\n\n" +
-                "Combined emphasis with **asterisks and _underscores_**.\n\n" +
-                "Strikethrough uses two tildes. ~Scratch this.~";
-        markwon.setMarkdown(binding.introductionTxt, introductionText);
+    }
 
-//        BookingTypeAdapter bookingTypeAdapter = new BookingTypeAdapter(BookingType.listDemo());
-//        binding.bookingTypeRv.setLayoutManager(new LinearLayoutManager(this));
-//        binding.bookingTypeRv.setAdapter(bookingTypeAdapter);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.app_bar_menu, menu);
 
-        String text = "Đặt phòng **theo giờ**: Huỷ miễn phí trước giờ nhận phòng **1 tiếng**.\n\n" +
-                "Đặt phòng **qua đêm**: Huỷ miễn phí trước giờ nhận phòng **2 tiếng**.\n\n" +
-                "Đặt phòng **theo ngày**: Huỷ miễn phí trước giờ nhận phòng **12 tiếng**.\n\n" +
-                "Lưu ý:\n\n" +
-                "- Có thể được huỷ miễn phí tong vòng **5 phút** kể từ thời điểm đặt phòng thành " +
-                "công nhưng thời điểm yêu cầu huỷ không được quá giờ nhận phòng.\n" +
-                "- Không cho phép huỷ phòng với phòng Flash Sale và Ưu đãi không hoàn huỷ.";
-        markwon.setMarkdown(binding.cancellationPolicyTxt, text);
+        for (int i = 0; i < menu.size(); i++) {
+            MenuItem item = menu.getItem(i);
 
-        appBarLayout = findViewById(R.id.appBarLayout);
-        toolbar = findViewById(R.id.toolbar);
-        final int colorStart = getColor(R.color.transparent);
-        final int colorEnd = getColor(R.color.white);
-
-        appBarLayout.setOnApplyWindowInsetsListener((v, insets) -> {
-            final WindowInsets systemWindowInsets = insets.consumeSystemWindowInsets();
-            v.setPadding(v.getPaddingLeft(), systemWindowInsets.getSystemWindowInsetTop(), v.getPaddingRight(), v.getPaddingBottom());
-            return systemWindowInsets;
-        });
-        toolbar.setNavigationOnClickListener(v -> finish());
+            if (item.getIcon() != null) {
+                item.getIcon().setTint(AppUtil.blendColors(getColor(R.color.white),
+                        getColor(R.color.icon), 0));
+            }
+        }
+        return true;
     }
 }
